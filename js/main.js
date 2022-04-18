18e48464ce1144237cedd75cc198c45505d42d43
KimGyuYeon <97150145+KimGyuYeon@users.noreply.github.com>
2022-04-18 19:34:28 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ $(function(){
         $('.skills-item2').addClass('on');
     })
 })
+
+//모바일 토글
+
+$(function(){
+    $('.toggle-btn').click(function(){
+        $('#gnb').addClass('on');
+    })
+
+    $('.close-btn').click(function(){
+        $('#gnb').removeClass('on');
+    })
+
+    $('.toggle-btn').click(function(){
+        $('.gnb-toggle-box').addClass('on');
+    })
+
+    $('.close-btn').click(function(){
+        $('.gnb-toggle-box').removeClass('on');
+    })
+
+    $('.gnb-toggle-box').click(function(){
+        $('#gnb').removeClass('on');
+    })
+
+    $('.gnb-toggle-box').click(function(){
+        $('.gnb-toggle-box').removeClass('on');
+    })
+})
